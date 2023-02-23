fceb4d884bf82a7496cda32b8f7ac0d3d918fbad
zekey <zekeyend7@gmail.com>
2023-02-23 05:53:00 -0800

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,6 +6,7 @@
  */
 void print_number(int n)
 {
+
 	unsigned int num = n;
 
 	if (n < 0)
@@ -15,5 +16,6 @@ void print_number(int n)
 	}
 	if ((num / 10) > 0)
 		print_number(num / 10);
+
 	_putchar((num % 10) + '0');
 }

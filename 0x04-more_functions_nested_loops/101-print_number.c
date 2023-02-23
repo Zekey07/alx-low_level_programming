c021d115003d51e08ed4f27420eec44faf471ab1
zekey <zekeyend7@gmail.com>
2023-02-23 06:04:51 -0800

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -11,11 +11,13 @@ void print_number(int n)
 
 	if (n < 0)
 	{
+		n *= -1;
+		num = n;
 		_putchar('-');
-		num = -num;
 	}
-	if ((num / 10) > 0)
-		print_number(num / 10);
+	num /= 10;
+	if ((num != 0)
+		print_number(num);
 
-	_putchar((num % 10) + '0');
+	_putchar((unsigned int) n % 10 + '0');
 }

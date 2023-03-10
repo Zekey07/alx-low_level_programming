c6c932db9dc80fa87496edd166147911b4fe4314
zekey <zekeyend7@gmail.com>
2023-03-10 12:34:50 -0800

    main.h

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -2,9 +2,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
-#include <ctype.h>
+#include <string.h>
 
-void _puts(char *s)
+int _putchar(char c)
 {
-	puts(s);
+	return putchar(c);
 }

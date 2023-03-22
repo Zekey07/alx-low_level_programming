e44833365409ed48b34341db2f80bf71c4595212
zekey <zekeyend7@gmail.com>
2023-03-22 02:08:02 -0700

    updates

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -1,13 +1,10 @@
-#include <unistd.h>
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
 
-/**
- * _putchar - writes the character c to stdout
- * _putchar - writes the character c to stdout
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
+int _isupper(int c)
 {
-	return (write(1, &c, 1));
+	return (c >= 'A' && c <= 'Z');
 }
